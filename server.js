56a5bafedbce2fdb87f472d97790209b65c7a764
Anubhav Srivastva <srivastvaanubhav.2212@gmail.com>
2026-06-19 04:53:04 +0530

    feat: add initial HTML structure for WebSocket server demo

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 import http from 'node:http';
 import dotenv from 'dotenv';
+import fs from 'node:fs/promises';
+import path from 'node:path';
 import { WebSocketServer } from 'ws';
 dotenv.config();
 const port=process.env.PORT || 3000
 
 const httpServer = http.createServer(async(req, res) => {
+    const indexFile=await fs.readFile(path.resolve('./index.html'),'utf-8');
+    res.setHeader('Content-Type', 'text/html');
+    return res.end(indexFile);
     });
 const wsServer=new WebSocketServer({server:httpServer});
 wsServer.on('connection',(websocket)=>{
