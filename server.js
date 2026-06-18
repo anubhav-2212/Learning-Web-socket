6755b4349c9baef237b46a98ca8f019dd873d0b0
Anubhav Srivastva <srivastvaanubhav.2212@gmail.com>
2026-06-19 05:07:43 +0530

    feat: add WebSocket client connection script to index.html and improve server logging

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import path from 'node:path';
 import { WebSocketServer } from 'ws';
 dotenv.config();
 const port=process.env.PORT || 3000
-
+//We are creating http server and returning index.html 
 const httpServer = http.createServer(async(req, res) => {
     const indexFile=await fs.readFile(path.resolve('./index.html'),'utf-8');
     res.setHeader('Content-Type', 'text/html');
@@ -13,12 +13,12 @@ const httpServer = http.createServer(async(req, res) => {
     });
 const wsServer=new WebSocketServer({server:httpServer});
 wsServer.on('connection',(websocket)=>{
-    console.log(`websocket connecting`)
+    console.log(`websocket connection`)
     websocket.on('message',(data)=>{
         console.log(`web socket message server `,data.toString())
     })
 })
 
-server.listen(port, () => {
+httpServer.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
