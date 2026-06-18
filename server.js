397b43c7d47eebfa810e0cd85b981c3751a8d843
Anubhav Srivastva <srivastvaanubhav.2212@gmail.com>
2026-06-18 19:11:14 +0530

    refactor: rename server variable to httpServer and initialize WebSocketServer

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 import http from 'node:http';
 import dotenv from 'dotenv';
+import { WebSocketServer } from 'ws';
 dotenv.config();
 const port=process.env.PORT || 3000
 
-const server = http.createServer(async(req, res) => {
-    
-});
+const httpServer = http.createServer(async(req, res) => {
+    });
+const wsServer=new WebSocketServer({server:httpServer});
 
 server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
